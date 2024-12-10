ca44ff7eed8ef3fb75b16bf67293c1a167e3e381
SiyueZhengzsy <sz4562@nyu.edu>
2024-12-09 20:40:50 -0500

    change remove

--- src/main/java/com/zs/foms/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/zs/foms/service/impl/CategoryServiceImpl.java
@@ -28,11 +28,12 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
      */
     @Override
     public void remove(Long id) {
+        System.out.println("ididid:"+id);
         LambdaQueryWrapper<Dish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
         //添加查询条件，根据分类id进行查询
         dishLambdaQueryWrapper.eq(Dish::getCategoryId,id);
         int count1 = dishService.count(dishLambdaQueryWrapper);
-
+        System.out.println("Count1:" + count1);
         //查询当前分类是否关联了菜品，如果已经关联，抛出一个业务异常
         if(count1 > 0){
             //已经关联菜品，抛出一个业务异常
@@ -43,7 +44,9 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> i
         LambdaQueryWrapper<Setmeal> setmealLambdaQueryWrapper = new LambdaQueryWrapper<>();
         //添加查询条件，根据分类id进行查询
         setmealLambdaQueryWrapper.eq(Setmeal::getCategoryId,id);
-        int count2 = setmealService.count();
+//        int count2 = setmealService.count();
+        int count2 =  setmealService.count(setmealLambdaQueryWrapper);
+        System.out.println("Count2:" + count2);
         if(count2 > 0){
             //已经关联套餐，抛出一个业务异常
             throw new CustomException("当前分类下关联了套餐，不能删除");
